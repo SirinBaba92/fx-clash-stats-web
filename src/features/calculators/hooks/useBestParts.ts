f05787fede66bb1fc215ac66c76828d5bdf7fb9d
SirinBaba92 <altuerk.baran@outlook.com>
2026-05-06 02:44:24 +0200

    Add optional focusStats parameter to useBestParts

--- src/features/calculators/hooks/useBestParts.ts
+++ src/features/calculators/hooks/useBestParts.ts
@@ -3,24 +3,24 @@ import useBestPart from './useBestPart';
 import useBestPartsStatsSum from './useBestPartsStatsSum';
 import type { BestParts } from '../types';
 
-const useBestParts = (): BestParts => {
+const useBestParts = (focusStats?: string[]): BestParts => {
   const brakes = useBrakes();
-  const bestBrake = useBestPart(brakes, 'brakes');
+  const bestBrake = useBestPart(brakes, 'brakes', focusStats);
 
   const engines = useEngines();
-  const bestEngine = useBestPart(engines, 'engines');
+  const bestEngine = useBestPart(engines, 'engines', focusStats);
 
   const frontWings = useFrontWings();
-  const bestFrontWing = useBestPart(frontWings, 'frontWings');
+  const bestFrontWing = useBestPart(frontWings, 'frontWings', focusStats);
 
   const gearboxes = useGearboxes();
-  const bestGearbox = useBestPart(gearboxes, 'gearboxes');
+  const bestGearbox = useBestPart(gearboxes, 'gearboxes', focusStats);
 
   const rearWings = useRearWings();
-  const bestRearWing = useBestPart(rearWings, 'rearWings');
+  const bestRearWing = useBestPart(rearWings, 'rearWings', focusStats);
 
   const suspensions = useSuspensions();
-  const bestSuspension = useBestPart(suspensions, 'suspensions');
+  const bestSuspension = useBestPart(suspensions, 'suspensions', focusStats);
 
   const sum = useBestPartsStatsSum({
     bestBrake,

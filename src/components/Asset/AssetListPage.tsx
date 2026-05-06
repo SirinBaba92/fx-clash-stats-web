c0660b5cf5f8e6881183bcbec63698f0992c225d
SirinBaba92 <altuerk.baran@outlook.com>
2026-05-06 03:45:53 +0200

    Implement reset button for collected assets
    Added a reset button for collected assets and updated the asset mapping.

--- src/components/Asset/AssetListPage.tsx
+++ src/components/Asset/AssetListPage.tsx
@@ -2,6 +2,7 @@ import { Alert, Container, Heading4 } from '@/components/ui';
 import { AssetHeadingContainer, AssetsFuseSearch, AssetsGrid, EditAssetCard } from '@/components/Asset';
 import { useTranslation } from 'react-i18next';
 import useAssetsFuseSearch from '@/hooks/useAssetsFuseSearch';
+import useCollectedAssetsStore from '@/store/collectedAssetsStore';
 import type { Asset } from '@/types';
 import type { CollectedAssetsKeys } from '@/store/collectedAssetsStore';
 
@@ -17,24 +18,43 @@ const AssetListPage = (props: Props) => {
   const { t } = useTranslation();
 
   const { filteredAssets, setSearch, search } = useAssetsFuseSearch(assets);
+  const resetCollectedAssets = useCollectedAssetsStore((state) => state.resetCollectedAssets);
+
+  const handleResetCollectedAssets = () => {
+    const shouldReset = window.confirm('Do you really want to reset all collected assets?');
+
+    if (shouldReset) {
+      resetCollectedAssets();
+    }
+  };
 
   return (
     <Container maxWidth='2xl'>
       <AssetHeadingContainer>
         <Heading4 className='m-0'>{title}</Heading4>
 
-        <AssetsFuseSearch onChangeSearch={setSearch} />
+        <div className='flex flex-col sm:flex-row gap-2'>
+          <button
+            className='text-white bg-red-700 hover:bg-red-800 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700'
+            onClick={handleResetCollectedAssets}
+            type='button'
+          >
+            Reset all
+          </button>
+
+          <AssetsFuseSearch onChangeSearch={setSearch} />
+        </div>
       </AssetHeadingContainer>
 
       {filteredAssets.length === 0 ? (
         <Alert severity='info'>{t('noResultsFor', { search })}</Alert>
       ) : (
         <AssetsGrid>
-          {filteredAssets.map((brake) => (
+          {filteredAssets.map((asset) => (
             <EditAssetCard
-              asset={brake}
+              asset={asset}
               assetKey={assetKey}
-              key={brake.id}
+              key={asset.id}
             />
           ))}
         </AssetsGrid>

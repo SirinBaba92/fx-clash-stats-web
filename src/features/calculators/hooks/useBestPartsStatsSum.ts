eea453668dffa9524e15b00447203293814dab07
SirinBaba92 <altuerk.baran@outlook.com>
2026-05-12 02:22:38 +0200

    Add bestBattery to useBestPartsStatsSum calculations

--- src/features/calculators/hooks/useBestPartsStatsSum.ts
+++ src/features/calculators/hooks/useBestPartsStatsSum.ts
@@ -7,52 +7,53 @@ interface Props {
   bestGearbox: BestPart;
   bestRearWing: BestPart;
   bestSuspension: BestPart;
+  bestBattery: BestPart;
 }
 
 const useBestPartsStatsSum = (props: Props): BestPartsSum => {
-  const { bestBrake, bestEngine, bestFrontWing, bestGearbox, bestRearWing, bestSuspension } = props;
+  const { bestBattery, bestBrake, bestEngine, bestFrontWing, bestGearbox, bestRearWing, bestSuspension } = props;
 
   return {
     cornering:
-      1 +
       bestBrake.stat.cornering +
       bestEngine.stat.cornering +
       bestFrontWing.stat.cornering +
       bestGearbox.stat.cornering +
       bestRearWing.stat.cornering +
-      bestSuspension.stat.cornering,
+      bestSuspension.stat.cornering +
+      bestBattery.stat.cornering,
     pitStopTime:
-      1 +
       bestBrake.stat.pitStopTime +
       bestEngine.stat.pitStopTime +
       bestFrontWing.stat.pitStopTime +
       bestGearbox.stat.pitStopTime +
       bestRearWing.stat.pitStopTime +
-      bestSuspension.stat.pitStopTime,
+      bestSuspension.stat.pitStopTime +
+      bestBattery.stat.pitStopTime,
     powerUnit:
-      1 +
       bestBrake.stat.powerUnit +
       bestEngine.stat.powerUnit +
       bestFrontWing.stat.powerUnit +
       bestGearbox.stat.powerUnit +
       bestRearWing.stat.powerUnit +
-      bestSuspension.stat.powerUnit,
+      bestSuspension.stat.powerUnit +
+      bestBattery.stat.powerUnit,
     qualifying:
-      1 +
       bestBrake.stat.qualifying +
       bestEngine.stat.qualifying +
       bestFrontWing.stat.qualifying +
       bestGearbox.stat.qualifying +
       bestRearWing.stat.qualifying +
-      bestSuspension.stat.qualifying,
+      bestSuspension.stat.qualifying +
+      bestBattery.stat.qualifying,
     speed:
-      1 +
       bestBrake.stat.speed +
       bestEngine.stat.speed +
       bestFrontWing.stat.speed +
       bestGearbox.stat.speed +
       bestRearWing.stat.speed +
-      bestSuspension.stat.speed,
+      bestSuspension.stat.speed +
+      bestBattery.stat.speed,
   };
 };
 
